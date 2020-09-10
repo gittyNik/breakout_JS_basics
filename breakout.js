b56131ebd6e670ab19558ce1b89e4047f15dd2f8
niskarsh <niskarshk@gmail.com>
2020-09-10 12:40:12 +0530

    after breakout

---
+++ breakout.js
@@ -0,0 +1,162 @@
+// variables, 
+
+// var, let
+
+// let variable_name = "variable_value"
+// let a= 20
+// console.log(typeof variable_name)
+// let boolean_variable;
+
+// scope
+{
+	//parent
+
+	let a=20;
+	
+	const b =30;
+	b=20;
+	{
+		// child 1
+		b
+
+
+
+	}
+b
+
+	{ // another child scope 2
+		let b=22;
+
+
+
+
+	}
+
+}
+
+// Global scope
+let b=23;
+
+// Functional scope
+function fn_name () {
+	for (let i=0;;) {
+
+	}
+
+	for (var j=0;;) {
+		
+	}
+
+	i, j
+}
+
+
+// conditionals
+
+// if else
+if (condition) {
+
+} else {
+
+}
+
+if (condition) {
+
+} else if(condition 2) {
+
+} else {
+
+}
+
+
+// Switch
+
+let variable_name= "possible value 1"
+switch (variable_name) {
+	case 1:
+	// break;
+	case "possible value 2":
+	break;
+	default:
+	break;
+
+}
+// fall through
+
+// ternary
+
+let value = (condition) ? (truthy): (falsy)
+
+let value = 1 < 2 ? false: true
+" sunit"
+"sunit "
+let trimmed_value = isTrim(variable)? trim(variable): variable
+
+
+// loops
+
+// for loop 
+
+for ( i=0; i <10   ; i ++) {
+	// loop body
+
+}
+
+
+
+//while loop
+ while (!condition1) {
+ 	// loop body
+ }
+
+ // do while 
+ do {
+
+ } while (condition)
+
+
+// arrays
+
+let ar = [1,2, 9 , 8]
+
+ar[2] = "kjljljljl"
+
+
+ar.length
+
+ar.pop()
+ar.push()
+ar.map()
+ar.filter()
+ar.reduce()
+ar.splice()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,109 @@
+// letiables, datatypes
+
+var letiable_name=988;
+let arc = "sdfhkj"
+
+
+// Scopes
+
+let dfg
+
+{
+
+	let a;
+	{
+		a //accesible
+		let b
+		{
+			let a
+			let b
+			let c
+		}
+		let c
+
+
+	}
+	b // not accessble
+
+	{	
+		b // not accessble
+		c // not accessible
+
+	}
+
+
+}
+
+
+a // not accesible
+
+b // not accessble
+
+function function_name(argument) {
+	let
+	{
+		for (var i=0;i<10; i++) {
+			
+	
+		}
+		i
+	}
+	i
+
+
+	// body...
+}
+
+// conditionals
+
+// IF-else
+
+if ("false") {
+	// if block
+} else {
+	// else block
+}
+
+name="Sam"
+switch (name) {
+	case "Dean":
+	kjkjhj
+		break;
+	case "Sam":
+	jhjkkjk
+		break;
+	case "Cassy":
+	jhkkjkj
+	 	break;
+ 	
+	default:
+		gjhgjhg
+		jhgjhghj
+}
+
+
+// loops
+
+// for loop
+
+for (let i=0; i<10; i++) {
+jhghjgjhgjhghj
+}
+
+let i;
+for  (;i<10;) {
+ i++;
+}
+
+while (i<10) {
+
+	jhgjhjhgjh
+}
+
+// Arrays
+
+let arr = [1, true, "string", {object: "object"}]
+
+
+arr[0]
+arr[3] = "kjlkjlkjlk"
